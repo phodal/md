bd479aba24ac6ab01e22a62045137f876d583ef3
Fengda HUANG <h@phodal.com>
2016-12-04 23:01:41 +0800

    fix for single line code highlight

--- js/showdown-plugins/showdown-prettify-for-wechat.js
+++ js/showdown-plugins/showdown-prettify-for-wechat.js
@@ -19,10 +19,11 @@
       type:   'output',
       filter: function (source) {
         return source.replace(/(<pre[^>]*>)?[\n\s]?<code([^>]*)>/gi, function (match, pre, codeClass) {
+          console.log(pre, codeClass);
           if (pre) {
             return '<pre class="prettyprint linenums" style="font-size: 10px;line-height: 12px"><code' + codeClass + ' style="font-size: 10px;line-height: 12px">';
           } else {
-            return ' <code class="prettyprint"  style="font-size: 10px;line-height: 12px">>';
+            return ' <code class="prettyprint code-in-text"  style="font-size: 16px;line-height: 18px">';
           }
         });
       }
