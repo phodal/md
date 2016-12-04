0ae20fae522fc074a58010db2ce423757b0c3d7a
Fengda HUANG <h@phodal.com>
2016-12-04 23:29:34 +0800

    thinking in md plugin

--- js/showdown-plugins/showdown-github-task-list.js
+++ js/showdown-plugins/showdown-github-task-list.js
@@ -14,16 +14,12 @@
 
 }(function (showdown) {
   'use strict';
-  showdown.extension('prettify', function () {
+  showdown.extension('tasklist', function () {
     return [{
       type:   'output',
       filter: function (source) {
-        return source.replace(/(<pre[^>]*>)?[\n\s]?<code([^>]*)>/gi, function (match, pre, codeClass) {
-          if (pre) {
-            return '<pre class="prettyprint linenums" style="font-size: 10px;line-height: 12px"><code' + codeClass + ' style="font-size: 10px;line-height: 12px">';
-          } else {
-            return ' <code class="prettyprint code-in-text"  style="font-size: 16px;line-height: 18px">';
-          }
+        return source.replace(/\[ \] /gi, function (match, pre) {
+          return '<li class="task-list-item-checkbox"' + source + '</div>'  ;
         });
       }
     }];
